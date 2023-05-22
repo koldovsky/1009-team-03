5256e090c4c075cc8f68498121445874e7446596
Alex Sychov <alexandersychev2005@gmail.com>
2023-05-22 12:21:14 +0000

    codewars

--- js_codewars/Task3.js
+++
@@ -1,4 +0,0 @@
-//Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
-function move(position, roll) {
-    return position + roll * 2;
-}
--- js_codewars/Task6.js
+++
@@ -1,4 +0,0 @@
-//Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
-function lovefunc(flower1, flower2) {
-    return (flower1 + flower2) % 2 !== 0;
-}
