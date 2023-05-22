eb046e46c145318305929778ec726aa7ebb7dc38
Alex Sychov <49594203+AlexanderSychev2005@users.noreply.github.com>
2023-05-22 18:05:12 +0300

    Update Lesson2.js

--- js_codewars/Lesson2.js
+++ js_codewars/Lesson2.js
@@ -69,7 +69,7 @@ function maps(x) {
 
 function firstNonConsecutive(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i + 1] != arr[i] + 1) {
+        if (arr[i + 1] !== arr[i] + 1) {
             return arr[i + 1];
         }
     }
