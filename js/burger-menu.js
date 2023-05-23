5228015717baa06e14e5306ad8490e9d0c2806bf
Alex Sychov <alexandersychev2005@gmail.com>
2023-05-23 23:22:38 +0000

    Added index.js file

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -5,8 +5,6 @@
     document.body.classList.toggle("lock");
     iconBurger.classList.toggle("active");
     navigationsBody.classList.toggle("active");
-    const marginNawigationWrapper = document.querySelector(".header__navigation-wrapper");
-    marginNawigationWrapper.style.marginTop = '100px'
   });
 
   const navLinks = document.querySelectorAll(".header__navigation__link");
@@ -16,8 +14,6 @@
         document.body.classList.remove("lock");
         iconBurger.classList.remove("active");
         navigationsBody.classList.remove("active");
-        const marginNawigationWrapper = document.querySelector(".header__navigation-wrapper");
-        marginNawigationWrapper.style.marginTop = '0px'
       }
     }
     );
