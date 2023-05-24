d5fa5a9cafdb0316fd60b93c41576e3356bc7322
Alex Sychov <alexandersychev2005@gmail.com>
2023-05-24 00:03:28 +0000

    Final version of burger menu with fixes

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,6 +1,6 @@
 (function () {
   const iconBurger = document.querySelector(".header__navigation-hamburger");
-  const navigationsBody = document.querySelector(".header__navigation-wrapper");
+  const navigationsBody = document.querySelector(".header__wrapper");
   iconBurger.addEventListener("click", function (e) {
     document.body.classList.toggle("lock");
     iconBurger.classList.toggle("active");
