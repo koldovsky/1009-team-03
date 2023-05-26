e167bf337a68a10b357c8d3f32e5556a29796607
Alex Sychov <alexandersychev2005@gmail.com>
2023-05-26 14:23:40 +0000

    js code fix

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,21 +1,19 @@
-(function () {
-  const Burger = document.querySelector(".header__hamburger");
-  const navigation_wrapper = document.querySelector(".header__wrapper");
-  Burger.addEventListener("click", function (e) {
-    navigation_wrapper.classList.toggle("active");
-    document.body.classList.toggle("lock");
-    Burger.classList.toggle("active");
-  });
+const burger = document.querySelector(".header__hamburger");
+const navigation_wrapper = document.querySelector(".header__wrapper");
+burger.addEventListener("click", function (e) {
+  navigation_wrapper.classList.toggle("active");
+  document.body.classList.toggle("lock");
+  burger.classList.toggle("active");
+});
 
-  const Link = document.querySelectorAll(".header__burger-menu-link");
-  Link.forEach((navLink) => {
-    navLink.addEventListener("click", (_) => {
-      if (Burger.classList.contains("active")) {
-        Burger.classList.remove("active");
-        navigation_wrapper.classList.remove("active");
-        document.body.classList.remove("lock");
-      }
+const link = document.querySelectorAll(".header__burger-menu-link");
+link.forEach((navLink) => {
+  navLink.addEventListener("click", (_) => {
+    if (burger.classList.contains("active")) {
+      burger.classList.remove("active");
+      navigation_wrapper.classList.remove("active");
+      document.body.classList.remove("lock");
     }
-    );
-  });
-})();
+  }
+  );
+});
