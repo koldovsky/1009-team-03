d63e7abb96c0f15066ce37439d42856ab713cf8b
Yan Zhylavy <yanzhylavy@gmail.com>
2023-05-30 12:17:03 +0300

    Added JS for block Shows

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("partialsLoaded", () => {
     import('./burger-menu.js');
     import('./testimonials.js');
+    import('./shows.js');
   });
---
+++ js/shows.js
@@ -0,0 +1,51 @@
+const carouselInner = document.querySelector('.shows__carousel-inner');
+const prevButton = document.querySelector('.shows__button--prev');
+const nextButton = document.querySelector('.shows__button--next');
+const caruoselCard = document.querySelector('.shows__carousel')
+
+let carouselPosition = 0;
+let carouselChange = 0;
+
+function checkWindowWidth() {
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth < 640) {
+        carouselChange = 235;
+    } else {
+        carouselChange = 370;
+    }
+}
+
+window.addEventListener('resize', checkWindowWidth);
+
+
+function moveCarousel() {
+    checkWindowWidth()
+    if (carouselPosition > -700) {
+        carouselPosition -= carouselChange;
+        carouselInner.style.transform = `translateX(${carouselPosition}px)`;
+    } else {
+        carouselPosition = 0;
+        carouselInner.style.transform = `translateX(${carouselPosition}px)`;
+    }
+};
+
+nextButton.addEventListener('click', moveCarousel)
+
+
+prevButton.addEventListener('click', () => {
+    checkWindowWidth()
+    if (carouselPosition < 0) {
+        carouselPosition += carouselChange;
+        carouselInner.style.transform = `translateX(${carouselPosition}px)`;
+    }
+});
+
+let autoUpdate = setInterval(moveCarousel, 2000);
+
+caruoselCard.addEventListener('mouseover', () => {
+    clearInterval(autoUpdate)
+});
+caruoselCard.addEventListener('mouseout', () => {
+    autoUpdate = setInterval(moveCarousel, 2000)
+});
